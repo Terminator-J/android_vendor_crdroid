2c5b96acdd84a529635b825c6a726959a1899f39
Terminator-J <terminator_j@hotbutteredmonkey.com>
2022-03-06 11:34:55 -0800

    soong: Backport TARGET_CAMERA_BOOTTIME_TIMESTAMP variable
    Helps get OnePlus Camera working for SDM845

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -45,6 +45,9 @@ type Product_variables struct {
 	Target_init_vendor_lib struct {
 		Whole_static_libs []string
 	}
+	Target_needs_camera_boottime struct {
+		Cppflags []string
+	}
 	Target_needs_netd_direct_connect_rule struct {
 		Cppflags []string
 	}
@@ -91,6 +94,7 @@ type ProductVariables struct {
 	Target_camera_needs_client_info  *bool `json:",omitempty"`
 	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
+	Target_needs_camera_boottime  *bool `json:",omitempty"`
 	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
